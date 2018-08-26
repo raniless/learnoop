c36a0b195b1347f36e187d42b5713b827c4a596c
raniless <40380021+raniless@users.noreply.github.com>
2018-08-26 23:08:22 +0900

    Test

--- javaoop/src/Start.java
+++ javaoop/src/Start.java
@@ -1,5 +1,3 @@
-
-
 public class Start {
 	public static void main(String[] args) {
 		System.out.println("Hello OOP!!!");
---
+++ javaoop/src/Start2.java
@@ -0,0 +1,9 @@
+public class Start2 {
+
+	public static void main(String[] args) {
+		int i;
+		i = 10;
+
+		double d = 20.0;
+	}
+}
