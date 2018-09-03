6d3ce5abfeeaa12f4440daa91b1fcf81f9af664c
raniless <40380021+raniless@users.noreply.github.com>
2018-09-04 00:03:00 +0900

    9/3 공부

---
+++ javaoop/src/ch03/reference/CallByReference2.java
@@ -0,0 +1,13 @@
+package ch03.reference;
+
+public class CallByReference2 {
+
+	public static void main(String[] args) {
+		Animal ref_a = new Animal();
+		Animal ref_b = ref_a;
+		
+		System.out.println(ref_a);
+		System.out.println(ref_b);
+	}
+
+}
