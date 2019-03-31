ec2ef72459b21a0853a62b3235ca7510df443971
Hyoki <Hyoki@192.168.219.104>
2019-03-31 22:56:30 +0900

    인스턴스 생성 Comment 추가

--- javaoop/src/ch03/abstraction01/MouseDriver.java
+++ javaoop/src/ch03/abstraction01/MouseDriver.java
@@ -4,6 +4,9 @@ public class MouseDriver {
 
 	public static void main(String[] args) {
 		Mouse mickey = new Mouse();
+		//Mouse mickey : Mouse 객체에 대한 참조 변수 mickey를 만든다.
+		//new Mouse()  : Mouse 클래스의 인스턴스를 하나 만들어서 Heap에 배치한다.
+		//대입문(=)      : Mouse 객체에 대한 주소(포인터)를 참조 변수 mickey에 할당한다.
 
 		mickey.name = "미키";
 		mickey.age  = 85;
